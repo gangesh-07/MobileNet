1b8494ef137931a3b5c9ed2182ebf15fc754a744
gangesh-07 <gangesh97dhar@gmail.com>
2021-12-08 13:29:35 -0500

    directory correction

--- mobilenet/Network.cpp
+++ mobilenet/Network.cpp
@@ -12,72 +12,72 @@ Network::Network()
 
 	m_Readdata = new ReadData( 224, 224, 3);
 
-    m_Layers_bn = new Layers_Bn(3, 32, 224, 2, "..\\binary_files\\conv1.weights", "..\\binary_files\\conv1_bn.mean", "..\\binary_files\\conv1_bn.variance", "..\\binary_files\\conv1_bn.filler",
-        "..\\binary_files\\conv1_bn.bias_filler");
+    m_Layers_bn = new Layers_Bn(3, 32, 224, 2, "binary_files\\conv1.weights", "binary_files\\conv1_bn.mean", "binary_files\\conv1_bn.variance", "binary_files\\conv1_bn.filler",
+        "binary_files\\conv1_bn.bias_filler");
 
-    m_Layers_ds2_1 = new Layers_Ds(32, 64, 112, 1, "..\\binary_files\\conv2_1_dw.weights", "..\\binary_files\\conv2_1_bn_dw.mean", "..\\binary_files\\conv2_1_bn_dw.variance", "..\\binary_files\\conv2_1_bn_dw.filler",
-        "..\\binary_files\\conv2_1_bn_dw.bias_filler", "..\\binary_files\\conv2_1_sep.weights", "..\\binary_files\\conv2_1_bn_sep.mean", "..\\binary_files\\conv2_1_bn_sep.variance", "..\\binary_files\\conv2_1_bn_sep.filler",
-        "..\\binary_files\\conv2_1_bn_sep.bias_filler");
+    m_Layers_ds2_1 = new Layers_Ds(32, 64, 112, 1, "binary_files\\conv2_1_dw.weights", "binary_files\\conv2_1_bn_dw.mean", "binary_files\\conv2_1_bn_dw.variance", "binary_files\\conv2_1_bn_dw.filler",
+        "binary_files\\conv2_1_bn_dw.bias_filler", "binary_files\\conv2_1_sep.weights", "binary_files\\conv2_1_bn_sep.mean", "binary_files\\conv2_1_bn_sep.variance", "binary_files\\conv2_1_bn_sep.filler",
+        "binary_files\\conv2_1_bn_sep.bias_filler");
 
-    m_Layers_ds2_2 = new Layers_Ds(64, 128, 112, 2, "..\\binary_files\\conv2_2_dw.weights", "..\\binary_files\\conv2_2_bn_dw.mean", "..\\binary_files\\conv2_2_bn_dw.variance", "..\\binary_files\\conv2_2_bn_dw.filler",
-        "..\\binary_files\\conv2_2_bn_dw.bias_filler", "..\\binary_files\\conv2_2_sep.weights", "..\\binary_files\\conv2_2_bn_sep.mean", "..\\binary_files\\conv2_2_bn_sep.variance", "..\\binary_files\\conv2_2_bn_sep.filler",
-        "..\\binary_files\\conv2_2_bn_sep.bias_filler");
+    m_Layers_ds2_2 = new Layers_Ds(64, 128, 112, 2, "binary_files\\conv2_2_dw.weights", "binary_files\\conv2_2_bn_dw.mean", "binary_files\\conv2_2_bn_dw.variance", "binary_files\\conv2_2_bn_dw.filler",
+        "binary_files\\conv2_2_bn_dw.bias_filler", "binary_files\\conv2_2_sep.weights", "binary_files\\conv2_2_bn_sep.mean", "binary_files\\conv2_2_bn_sep.variance", "binary_files\\conv2_2_bn_sep.filler",
+        "binary_files\\conv2_2_bn_sep.bias_filler");
 
-    m_Layers_ds3_1 = new Layers_Ds(128, 128, 56, 1, "..\\binary_files\\conv3_1_dw.weights", "..\\binary_files\\conv3_1_bn_dw.mean", "..\\binary_files\\conv3_1_bn_dw.variance", "..\\binary_files\\conv3_1_bn_dw.filler",
-        "..\\binary_files\\conv3_1_bn_dw.bias_filler", "..\\binary_files\\conv3_1_sep.weights", "..\\binary_files\\conv3_1_bn_sep.mean", "..\\binary_files\\conv3_1_bn_sep.variance", "..\\binary_files\\conv3_1_bn_sep.filler",
-        "..\\binary_files\\conv3_1_bn_sep.bias_filler");
+    m_Layers_ds3_1 = new Layers_Ds(128, 128, 56, 1, "binary_files\\conv3_1_dw.weights", "binary_files\\conv3_1_bn_dw.mean", "binary_files\\conv3_1_bn_dw.variance", "binary_files\\conv3_1_bn_dw.filler",
+        "binary_files\\conv3_1_bn_dw.bias_filler", "binary_files\\conv3_1_sep.weights", "binary_files\\conv3_1_bn_sep.mean", "binary_files\\conv3_1_bn_sep.variance", "binary_files\\conv3_1_bn_sep.filler",
+        "binary_files\\conv3_1_bn_sep.bias_filler");
 
-    m_Layers_ds3_2 = new Layers_Ds(128, 256, 56, 2, "..\\binary_files\\conv3_2_dw.weights", "..\\binary_files\\conv3_2_bn_dw.mean", "..\\binary_files\\conv3_2_bn_dw.variance", "..\\binary_files\\conv3_2_bn_dw.filler",
-        "..\\binary_files\\conv3_2_bn_dw.bias_filler", "..\\binary_files\\conv3_2_sep.weights", "..\\binary_files\\conv3_2_bn_sep.mean", "..\\binary_files\\conv3_2_bn_sep.variance", "..\\binary_files\\conv3_2_bn_sep.filler",
-        "..\\binary_files\\conv3_2_bn_sep.bias_filler");
+    m_Layers_ds3_2 = new Layers_Ds(128, 256, 56, 2, "binary_files\\conv3_2_dw.weights", "binary_files\\conv3_2_bn_dw.mean", "binary_files\\conv3_2_bn_dw.variance", "binary_files\\conv3_2_bn_dw.filler",
+        "binary_files\\conv3_2_bn_dw.bias_filler", "binary_files\\conv3_2_sep.weights", "binary_files\\conv3_2_bn_sep.mean", "binary_files\\conv3_2_bn_sep.variance", "binary_files\\conv3_2_bn_sep.filler",
+        "binary_files\\conv3_2_bn_sep.bias_filler");
 
-    m_Layers_ds4_1 = new Layers_Ds(256, 256, 28, 1, "..\\binary_files\\conv4_1_dw.weights", "..\\binary_files\\conv4_1_bn_dw.mean", "..\\binary_files\\conv4_1_bn_dw.variance", "..\\binary_files\\conv4_1_bn_dw.filler",
-        "..\\binary_files\\conv4_1_bn_dw.bias_filler", "..\\binary_files\\conv4_1_sep.weights", "..\\binary_files\\conv4_1_bn_sep.mean", "..\\binary_files\\conv4_1_bn_sep.variance", "..\\binary_files\\conv4_1_bn_sep.filler",
-        "..\\binary_files\\conv4_1_bn_sep.bias_filler");
+    m_Layers_ds4_1 = new Layers_Ds(256, 256, 28, 1, "binary_files\\conv4_1_dw.weights", "binary_files\\conv4_1_bn_dw.mean", "binary_files\\conv4_1_bn_dw.variance", "binary_files\\conv4_1_bn_dw.filler",
+        "binary_files\\conv4_1_bn_dw.bias_filler", "binary_files\\conv4_1_sep.weights", "binary_files\\conv4_1_bn_sep.mean", "binary_files\\conv4_1_bn_sep.variance", "binary_files\\conv4_1_bn_sep.filler",
+        "binary_files\\conv4_1_bn_sep.bias_filler");
 
-    m_Layers_ds4_2 = new Layers_Ds(256, 512, 28, 2, "..\\binary_files\\conv4_2_dw.weights", "..\\binary_files\\conv4_2_bn_dw.mean", "..\\binary_files\\conv4_2_bn_dw.variance", "..\\binary_files\\conv4_2_bn_dw.filler",
-        "..\\binary_files\\conv4_2_bn_dw.bias_filler", "..\\binary_files\\conv4_2_sep.weights", "..\\binary_files\\conv4_2_bn_sep.mean", "..\\binary_files\\conv4_2_bn_sep.variance", "..\\binary_files\\conv4_2_bn_sep.filler",
-        "..\\binary_files\\conv4_2_bn_sep.bias_filler");
+    m_Layers_ds4_2 = new Layers_Ds(256, 512, 28, 2, "binary_files\\conv4_2_dw.weights", "binary_files\\conv4_2_bn_dw.mean", "binary_files\\conv4_2_bn_dw.variance", "binary_files\\conv4_2_bn_dw.filler",
+        "binary_files\\conv4_2_bn_dw.bias_filler", "binary_files\\conv4_2_sep.weights", "binary_files\\conv4_2_bn_sep.mean", "binary_files\\conv4_2_bn_sep.variance", "binary_files\\conv4_2_bn_sep.filler",
+        "binary_files\\conv4_2_bn_sep.bias_filler");
 
-    m_Layers_ds5_1 = new Layers_Ds(512, 512, 14, 1, "..\\binary_files\\conv5_1_dw.weights", "..\\binary_files\\conv5_1_bn_dw.mean", "..\\binary_files\\conv5_1_bn_dw.variance", "..\\binary_files\\conv5_1_bn_dw.filler",
-        "..\\binary_files\\conv5_1_bn_dw.bias_filler", "..\\binary_files\\conv5_1_sep.weights", "..\\binary_files\\conv5_1_bn_sep.mean", "..\\binary_files\\conv5_1_bn_sep.variance", "..\\binary_files\\conv5_1_bn_sep.filler",
-        "..\\binary_files\\conv5_1_bn_sep.bias_filler");
+    m_Layers_ds5_1 = new Layers_Ds(512, 512, 14, 1, "binary_files\\conv5_1_dw.weights", "binary_files\\conv5_1_bn_dw.mean", "binary_files\\conv5_1_bn_dw.variance", "binary_files\\conv5_1_bn_dw.filler",
+        "binary_files\\conv5_1_bn_dw.bias_filler", "binary_files\\conv5_1_sep.weights", "binary_files\\conv5_1_bn_sep.mean", "binary_files\\conv5_1_bn_sep.variance", "binary_files\\conv5_1_bn_sep.filler",
+        "binary_files\\conv5_1_bn_sep.bias_filler");
 
-    m_Layers_ds5_2 = new Layers_Ds(512, 512, 14, 1, "..\\binary_files\\conv5_2_dw.weights", "..\\binary_files\\conv5_2_bn_dw.mean", "..\\binary_files\\conv5_2_bn_dw.variance", "..\\binary_files\\conv5_2_bn_dw.filler",
-        "..\\binary_files\\conv5_2_bn_dw.bias_filler", "..\\binary_files\\conv5_2_sep.weights", "..\\binary_files\\conv5_2_bn_sep.mean", "..\\binary_files\\conv5_2_bn_sep.variance", "..\\binary_files\\conv5_2_bn_sep.filler",
-        "..\\binary_files\\conv5_2_bn_sep.bias_filler");
+    m_Layers_ds5_2 = new Layers_Ds(512, 512, 14, 1, "binary_files\\conv5_2_dw.weights", "binary_files\\conv5_2_bn_dw.mean", "binary_files\\conv5_2_bn_dw.variance", "binary_files\\conv5_2_bn_dw.filler",
+        "binary_files\\conv5_2_bn_dw.bias_filler", "binary_files\\conv5_2_sep.weights", "binary_files\\conv5_2_bn_sep.mean", "binary_files\\conv5_2_bn_sep.variance", "binary_files\\conv5_2_bn_sep.filler",
+        "binary_files\\conv5_2_bn_sep.bias_filler");
 
-    m_Layers_ds5_3 = new Layers_Ds(512, 512, 14, 1, "..\\binary_files\\conv5_3_dw.weights", "..\\binary_files\\conv5_3_bn_dw.mean", "..\\binary_files\\conv5_3_bn_dw.variance", "..\\binary_files\\conv5_3_bn_dw.filler",
-        "..\\binary_files\\conv5_3_bn_dw.bias_filler", "..\\binary_files\\conv5_3_sep.weights", "..\\binary_files\\conv5_3_bn_sep.mean", "..\\binary_files\\conv5_3_bn_sep.variance", "..\\binary_files\\conv5_3_bn_sep.filler",
-        "..\\binary_files\\conv5_3_bn_sep.bias_filler");
+    m_Layers_ds5_3 = new Layers_Ds(512, 512, 14, 1, "binary_files\\conv5_3_dw.weights", "binary_files\\conv5_3_bn_dw.mean", "binary_files\\conv5_3_bn_dw.variance", "binary_files\\conv5_3_bn_dw.filler",
+        "binary_files\\conv5_3_bn_dw.bias_filler", "binary_files\\conv5_3_sep.weights", "binary_files\\conv5_3_bn_sep.mean", "binary_files\\conv5_3_bn_sep.variance", "binary_files\\conv5_3_bn_sep.filler",
+        "binary_files\\conv5_3_bn_sep.bias_filler");
 
-    m_Layers_ds5_4 = new Layers_Ds(512, 512, 14, 1, "..\\binary_files\\conv5_4_dw.weights", "..\\binary_files\\conv5_4_bn_dw.mean", "..\\binary_files\\conv5_4_bn_dw.variance", "..\\binary_files\\conv5_4_bn_dw.filler",
-        "..\\binary_files\\conv5_4_bn_dw.bias_filler", "..\\binary_files\\conv5_4_sep.weights", "..\\binary_files\\conv5_4_bn_sep.mean", "..\\binary_files\\conv5_4_bn_sep.variance", "..\\binary_files\\conv5_4_bn_sep.filler",
-        "..\\binary_files\\conv5_4_bn_sep.bias_filler");
+    m_Layers_ds5_4 = new Layers_Ds(512, 512, 14, 1, "binary_files\\conv5_4_dw.weights", "binary_files\\conv5_4_bn_dw.mean", "binary_files\\conv5_4_bn_dw.variance", "binary_files\\conv5_4_bn_dw.filler",
+        "binary_files\\conv5_4_bn_dw.bias_filler", "binary_files\\conv5_4_sep.weights", "binary_files\\conv5_4_bn_sep.mean", "binary_files\\conv5_4_bn_sep.variance", "binary_files\\conv5_4_bn_sep.filler",
+        "binary_files\\conv5_4_bn_sep.bias_filler");
 
-    m_Layers_ds5_5 = new Layers_Ds(512, 512, 14, 1, "..\\binary_files\\conv5_5_dw.weights", "..\\binary_files\\conv5_5_bn_dw.mean", "..\\binary_files\\conv5_5_bn_dw.variance", "..\\binary_files\\conv5_5_bn_dw.filler",
-        "..\\binary_files\\conv5_5_bn_dw.bias_filler", "..\\binary_files\\conv5_5_sep.weights", "..\\binary_files\\conv5_5_bn_sep.mean", "..\\binary_files\\conv5_5_bn_sep.variance", "..\\binary_files\\conv5_5_bn_sep.filler",
-        "..\\binary_files\\conv5_5_bn_sep.bias_filler");
+    m_Layers_ds5_5 = new Layers_Ds(512, 512, 14, 1, "binary_files\\conv5_5_dw.weights", "binary_files\\conv5_5_bn_dw.mean", "binary_files\\conv5_5_bn_dw.variance", "binary_files\\conv5_5_bn_dw.filler",
+        "binary_files\\conv5_5_bn_dw.bias_filler", "binary_files\\conv5_5_sep.weights", "binary_files\\conv5_5_bn_sep.mean", "binary_files\\conv5_5_bn_sep.variance", "binary_files\\conv5_5_bn_sep.filler",
+        "binary_files\\conv5_5_bn_sep.bias_filler");
 
-    m_Layers_ds6 = new Layers_Ds(512, 1024, 14, 2, "..\\binary_files\\conv6_dw.weights", "..\\binary_files\\conv6_bn_dw.mean", "..\\binary_files\\conv6_bn_dw.variance", "..\\binary_files\\conv6_bn_dw.filler",
-        "..\\binary_files\\conv6_bn_dw.bias_filler", "..\\binary_files\\conv6_sep.weights", "..\\binary_files\\conv6_bn_sep.mean", "..\\binary_files\\conv6_bn_sep.variance", "..\\binary_files\\conv6_bn_sep.filler",
-        "..\\binary_files\\conv6_bn_sep.bias_filler");
+    m_Layers_ds6 = new Layers_Ds(512, 1024, 14, 2, "binary_files\\conv6_dw.weights", "binary_files\\conv6_bn_dw.mean", "binary_files\\conv6_bn_dw.variance", "binary_files\\conv6_bn_dw.filler",
+        "binary_files\\conv6_bn_dw.bias_filler", "binary_files\\conv6_sep.weights", "binary_files\\conv6_bn_sep.mean", "binary_files\\conv6_bn_sep.variance", "binary_files\\conv6_bn_sep.filler",
+        "binary_files\\conv6_bn_sep.bias_filler");
 
-    m_Layers_ds7 = new Layers_Ds(1024, 1024, 7, 1, "..\\binary_files\\conv7_dw.weights", "..\\binary_files\\conv7_bn_dw.mean", "..\\binary_files\\conv7_bn_dw.variance", "..\\binary_files\\conv7_bn_dw.filler",
-        "..\\binary_files\\conv7_bn_dw.bias_filler", "..\\binary_files\\conv7_sep.weights", "..\\binary_files\\conv7_bn_sep.mean", "..\\binary_files\\conv7_bn_sep.variance", "..\\binary_files\\conv7_bn_sep.filler",
-        "..\\binary_files\\conv7_bn_sep.bias_filler");
+    m_Layers_ds7 = new Layers_Ds(1024, 1024, 7, 1, "binary_files\\conv7_dw.weights", "binary_files\\conv7_bn_dw.mean", "binary_files\\conv7_bn_dw.variance", "binary_files\\conv7_bn_dw.filler",
+        "binary_files\\conv7_bn_dw.bias_filler", "binary_files\\conv7_sep.weights", "binary_files\\conv7_bn_sep.mean", "binary_files\\conv7_bn_sep.variance", "binary_files\\conv7_bn_sep.filler",
+        "binary_files\\conv7_bn_sep.bias_filler");
 
     m_Poollayer = new GlobalPoolLayer(1024, 7);
 
-    //m_Convlayer7 = new ConvLayer("..\\binary_files\\fc7.weights", 1024, 12, 1, 1, 0, 1, 1, "..\\binary_files\\fc7.biases");
-    m_Fclayer = new FcLayer("..\\binary_files\\fc.weights", "..\\binary_files\\fc.biases", 1024, 325);
+    //m_Convlayer7 = new ConvLayer("binary_files\\fc7.weights", 1024, 12, 1, 1, 0, 1, 1, "binary_files\\fc7.biases");
+    m_Fclayer = new FcLayer("binary_files\\fc.weights", "binary_files\\fc.biases", 1024, 325);
 
  
     Softmax = new SoftmaxLayer(325);
 
 	
-    ifstream input("..\\class_list.txt");
-    //input.open("..\\class_list.txt");
+    ifstream input("class_list.txt");
+    //input.open("class_list.txt");
     string line,temp;
     const char* str;
     int c = 0;
